e2b2195a888003f4b7c25c89e60431d0842bb816
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-08-29 00:00:35 +0200

    chore: add commons compress

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,4 +13,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
     implementation("com.android.tools.build:gradle:7.2.2")
     implementation("org.jetbrains.compose:compose-gradle-plugin:1.2.0-alpha01-dev764")
+    implementation("org.apache.commons:commons-compress:1.21")
 }
