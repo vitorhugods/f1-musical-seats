1ea29ccc0e339d6167cf0e052580d6e91ad3871a
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-08-29 02:17:06 +0200

    feat(web): initial MVP

--- web/src/jsMain/kotlin/Main.kt
+++ web/src/jsMain/kotlin/Main.kt
@@ -1,14 +1,18 @@
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.web.css.*
-import org.jetbrains.compose.web.dom.*
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Img
+import org.jetbrains.compose.web.dom.Span
+import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 import xyz.schwaab.f1seats.data.SeatRepository
 import xyz.schwaab.f1seats.dto.Driver
+import xyz.schwaab.f1seats.dto.Team
 import xyz.schwaab.f1seats.dto.TeamDrivers
 
 fun main() {
@@ -27,18 +31,49 @@ fun main() {
 
 @Composable
 private fun screen(response: List<TeamDrivers>) {
-    response.flatMap { it.drivers }.forEach { driver ->
-        DriverCard(driver)
+    response.forEach { teamInfo ->
+        TeamInfo(teamInfo.team)
+        Div({
+            style {
+                display(DisplayStyle.InlineBlock)
+            }
+        }) {
+            teamInfo.drivers.forEach { driver ->
+                DriverCard(driver)
+            }
+        }
     }
 }
 
 @Composable
-private fun DriverCard(driver: Driver){
+private fun DriverCard(driver: Driver) {
     Div({ style { padding(25.px) } }) {
-        Img("http://${driver.photoUrl}")
+        Img(driver.photoUrl) {
+            style {
+                maxHeight(100.px)
+            }
+        }
         Span() {
             Text(driver.name)
         }
     }
 }
 
+@Composable
+private fun TeamInfo(team: Team) {
+    Div({
+        style {
+            padding(25.px)
+            display(DisplayStyle.InlineBlock)
+        }
+    }) {
+        Span() {
+            Text(team.name)
+        }
+        Img(team.logoUrl) {
+            style {
+                this.maxHeight(50.px)
+            }
+        }
+    }
+}
